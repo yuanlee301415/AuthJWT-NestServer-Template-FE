f57d6f2cd85e1d28cd4e44942a78719e88f3ccb3
leeyuan <liyuan520310@163.com>
2021-05-16 16:37:18 +0800

    clean

---
+++ src/views/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="home" style="text-align: center;">
+        <img alt="Vue logo" src="@/assets/logo.png">
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
